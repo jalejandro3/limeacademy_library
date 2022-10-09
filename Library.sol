a5bab55101e1535f17dcc73e5e8dba7fd329d717
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2022-10-09 17:49:52 -0300

    Add logic to add books to the library

--- Library.sol
+++ Library.sol
@@ -5,4 +5,24 @@ pragma abicoder v2;
 import "./Ownable.sol";
 
 contract Library is Ownable {
+    mapping(string => uint) public libraryBooks;
+
+    struct Book {
+        string title;
+        uint qty;
+    }
+
+    struct User {
+        address _address;
+    }
+
+    event LogLibraryBooks(Book[] books);
+    event LogAddedLibraryBook(string title, uint qty);
+    event LogUserList(User userList);
+
+    function addBook(Book calldata book) public onlyOwner {
+        require(book.qty > 0, "You must add at least one copy.");
+        libraryBooks[book.title] += book.qty;
+        emit LogAddedLibraryBook(book.title, book.qty);
+    }
 }
